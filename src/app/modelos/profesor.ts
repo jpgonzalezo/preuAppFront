73c9c0701e3ff32edb9f7c6ab17a18f2e798145d
jpgonzalezo <juan.gonzalez.o@usach.cl>
2019-04-15 20:16:23 -0400

    ADD: Modelo profesor

--- src/app/modelos/profesor.ts
+++ src/app/modelos/profesor.ts
@@ -1,88 +1,15 @@
+import { Direccion } from 'src/app/modelos/direccion.model';
+import { Asignatura } from 'src/app/modelos/asignatura.model';
 export interface Profesor{
-    id: number,
-    nombres: string;
-    apellido_paterno: string;
-    apellido_materno: string;
-    email: string;
-    password: string;
-    telefono: string;
-    rut: string;
-    direccion: string;
-    asignatura: string;
+  id: string,
+  apellido_paterno: string,
+  nombres: string,
+  password: string,
+  direccion: Direccion,
+  rut: string,
+  email: string,
+  telefono: string,
+  apellido_materno: string,
+  asignatura: Asignatura,
+  imagen: string
 }
-
-export const PROFESOR_DATA: Profesor[]=[
-    {
-      id: 1,
-      nombres: "Juan Pablo",
-      apellido_paterno: "Gonzalez",
-      apellido_materno: "Opazo",
-      email: "jpgonzalez@gmail.com",
-      password: "123",
-      telefono: "+56974459133",
-      rut: "18.883.307-1",
-      direccion: "cale 234, Sn Bdo",
-      asignatura: "Matemáticas",
-    },
-    {
-      id:2,
-      nombres: "Juan Pablo",
-      apellido_paterno: "Gonzalez",
-      apellido_materno: "Opazo",
-      email: "jpgonzalez@gmail.com",
-      password: "123",
-      telefono: "+56974459133",
-      rut: "18.883.307-1",
-      direccion: "cale 234, Sn Bdo",
-      asignatura: "Matemáticas",
-    },
-    {
-      id:3,
-      nombres: "Juan Pablo",
-      apellido_paterno: "Gonzalez",
-      apellido_materno: "Opazo",
-      email: "jpgonzalez@gmail.com",
-      password: "123",
-      telefono: "+56974459133",
-      rut: "18.883.307-1",
-      direccion: "cale 234, Sn Bdo",
-      asignatura: "Matemáticas",
-    },
-    {
-      id:4,
-      nombres: "Juan Pablo",
-      apellido_paterno: "Gonzalez",
-      apellido_materno: "Opazo",
-      email: "jpgonzalez@gmail.com",
-      password: "123",
-      telefono: "+56974459133",
-      rut: "18.883.307-1",
-      direccion: "cale 234, Sn Bdo",
-      asignatura: "Matemáticas",
-    },
-    {
-      id:5,
-      nombres: "Juan Pablo",
-      apellido_paterno: "Gonzalez",
-      apellido_materno: "Opazo",
-      email: "jpgonzalez@gmail.com",
-      password: "123",
-      telefono: "+56974459133",
-      rut: "18.883.307-1",
-      direccion: "cale 234, Sn Bdo",
-      asignatura: "Matemáticas",
-    },
-    {
-      id:6,
-      nombres: "Juan Pablo",
-      apellido_paterno: "Gonzalez",
-      apellido_materno: "Opazo",
-      email: "jpgonzalez@gmail.com",
-      password: "123",
-      telefono: "+56974459133",
-      rut: "18.883.307-1",
-      direccion: "cale 234, Sn Bdo",
-      asignatura: "Matemáticas",
-    },
-  ]
-  
