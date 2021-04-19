752ebe77ab7aace3e44124872d0df9de2540f55f
lmigryk <luis.migryk@usach.cl>
2021-04-19 16:30:55 -0400

    feat: angular no esta soportando ngModel

--- src/app/componentes/alumno/hoja-respuesta/hoja-respuesta.component.ts
+++ src/app/componentes/alumno/hoja-respuesta/hoja-respuesta.component.ts
@@ -1,61 +1,80 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import {MatPaginator, MatTableDataSource} from '@angular/material';
+import { MatTableDataSource } from '@angular/material';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface Respuesta {
+  numero_pregunta: number,
+  alternativa: string
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 12, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 13, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 14, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 15, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 16, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 17, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 18, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 19, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 20, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 21, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 22, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 23, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 24, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 25, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 26, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 27, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 28, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 29, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 30, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+export interface Header {
+  text: string,
+  value: string
+}
 
 @Component({
   selector: 'app-hoja-respuesta',
   templateUrl: './hoja-respuesta.component.html',
   styleUrls: ['./hoja-respuesta.component.css']
 })
-export class HojaRespuestaComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+
+export class HojaRespuestaComponent implements OnInit {
+  preguntas: [];
+  respuestas: Respuesta[];
+  pageRespuesta: number;
+  pageSizeRespuesta: number;
+  collectionSizeRespuesta: number;
+  headers: Header[]
+
+
 
   ngOnInit() {
-    this.dataSource.paginator = this.paginator;
+    this.creaJsonRespuesta()
   }
-  
-  constructor() { }
+
+  constructor() {
+    this.respuestas = []
+    this.pageRespuesta = 1
+    this.pageSizeRespuesta = 10
+    this.collectionSizeRespuesta = 70
+    this.headers = [
+      {
+        text: "Nro Pregunta",
+        value: "name"
+      },
+      { text: "A", value: "A" },
+      { text: "B", value: "B" },
+      { text: "C", value: "C" },
+      { text: "D", value: "D" },
+      { text: "E", value: "E" }
+    ]
+  }
+
+  onRadioButtonClick(){
+    console.log(this.respuestas)
+  }
+
+  creaJsonRespuesta() {
+    var preguntas = Array.from(Array(70).keys())
+    console.log(preguntas)
+    var array = []
+    var i = 0
+    preguntas.forEach(function () {
+      var json = {
+        numero_pregunta: i + 1,
+        alternativa: ""
+      }
+      array.push(json)
+      i = i + 1
+    });
+    this.respuestas = array
+    console.log(this.respuestas)
+  }
+  get respuestas_tabla(): any[] {
+    return this.respuestas
+      .map((pregunta, i) => ({ id: i, ...pregunta }))
+      .slice((this.pageRespuesta - 1) * this.pageSizeRespuesta, (this.pageRespuesta - 1) * this.pageSizeRespuesta + this.pageSizeRespuesta);
+  }
 
 }
