fe78178c3ac4a07069c4c35260ae28a5d57e5122
jgonza57-at-483819493476 <jgonza57@bancoestado.cl>
2021-05-30 00:06:57 -0400

    FIX: Ruta api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   sessionTime: 14400000,
-  API_SERVER_URL: 'https://preusanbernardo.cl/app1'
+  API_SERVER_URL: 'http://preuapp.preusanbernardo.cl'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   sessionTime: 14400000,
-  API_SERVER_URL: 'https://preusanbernardo.cl/app1'
+  API_SERVER_URL: 'http://preuapp.preusanbernardo.cl'
 };
 
 /*
