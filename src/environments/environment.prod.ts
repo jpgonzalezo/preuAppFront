0312cf9e09c34367f7ffd7fc633da6114e37ddd0
jgonza57-at-483819493476 <jgonza57@bancoestado.cl>
2021-05-29 23:29:25 -0400

    UPDATE: Environment API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   sessionTime: 14400000,
-  API_SERVER_URL: 'http://localhost:5000'
+  API_SERVER_URL: 'https://preusanbernardo.cl/app1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   sessionTime: 14400000,
-  API_SERVER_URL: 'http://localhost:5000'
+  API_SERVER_URL: 'https://preusanbernardo.cl/app1'
 };
 
 /*
